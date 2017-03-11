43088e8dd31a2affd89cdd29d2a55b4c8d47ff1f
Joe Bosman <joseph.a.bosman@gmail.com>
2017-03-10 19:25:43 -0500

    Added quick express server.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+// Statically serve public folder
+app.use( '/public', express.static( path.join( __dirname, 'public') ));
+
+// Make sure any routes direct them to the index page
+app.get('*', (req,res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen( port, () => {
+	console.log("Listening on port " + port);
+
+});
