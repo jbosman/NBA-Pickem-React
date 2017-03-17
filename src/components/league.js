ca61c4c27c3dc28263df08e5a2ce3dd1179fdee3
Joe Bosman <joseph.a.bosman@gmail.com>
2017-03-17 11:45:03 -0400

    Updated league view to list teams in descending order.

--- src/components/league.js
+++ src/components/league.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router';
 import { fetchLeague } from '../actions/league.action';
 import { selectTeam } from '../actions/team.action';
 import { fetchNBATeamInfo } from '../actions/nba.team.action';
+import _ from 'lodash';
 
 class League extends Component {
 
@@ -25,15 +26,26 @@ class League extends Component {
 	}
 
 	renderTeamsInLeague(){
+
+		// Create an array of objects which contains the individual team names and their total number of wins
+		// Special note here for the leagueId. This is needed to render the correct team when it's clicked on.
+		// Because the teams are being sorted by wins here we loose the original database order.
+		let league_teams_objs = this.props.teams.map( (team, i) => {
+			return { name: team.name, wins: this.sumTeamWins(team), leagueId: i };
+		});
+
+		// Sort those objects in descending order
+		let league_teams_objs_sorted = _.orderBy( league_teams_objs, "wins", "desc" );
+
 		return ( 
-			this.props.teams.map( (team, i) => {
+			league_teams_objs_sorted.map( (team, i) => {
 				return (
 					<div className='row' key={i}>
 						<div className='col-lg-6'> 
-							<a onClick={this.updateTeam.bind(this, i)}>{ team.name }'s Team</a>
+							<a onClick={this.updateTeam.bind(this, team.leagueId)}>{ team.name }'s Team</a>
 						</div>
 						<div className='col-lg-6'>
-							{ this.sumTeamWins(team) }
+							{ team.wins }
 						</div>
 					</div>
 				)
